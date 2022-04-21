e16ec8a5887b6ed3946dd9aba8adb730f4f8746e
AnshuMan0423 <100996409+AnshuMan0423@users.noreply.github.com>
2022-04-21 08:51:56 +0530

    Add files via upload

---
+++ Practice/src/basics/A.java
@@ -0,0 +1,15 @@
+package basics;
+
+public class A {
+	static int miles = 100;
+	static double kilometers = miles * 1.6;
+	
+	
+	
+	public static void main(String[] args) {
+		System.out.println(miles + " miles in kilometers is " + kilometers);
+		
+	}
+	
+
+}
---
+++ Practice/src/basics/B.java
@@ -0,0 +1,22 @@
+package basics;
+
+public class B {
+	
+	
+	public static void main(String[] args) {
+		int i = 10;
+		int j;
+	{
+			
+		System.out.println(i);
+		j = 20;
+		i = 40;
+		
+		System.out.println(i * j);
+		
+		
+	}
+		
+	}
+
+}
